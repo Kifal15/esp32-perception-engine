2df67a8f343d76baf68e0d11e79d21f0f6b30f47
kifal15 <kifal531@gmail.com>
2026-08-13 12:13:51 +0800

    Add register helpers and TMF8829 power-up sequence

--- main/main.c
+++ main/main.c
@@ -3,7 +3,7 @@
 #include "freertos/task.h"
 #include "esp_err.h"
 #include "driver/i2c_master.h"
-
+#include <stdbool.h>
 
 #define I2C_PORT_NUM 0 
 #define GPIO_NUM_SDA 21
@@ -12,14 +12,23 @@
 #define TOF_ADDRESS 0x41 
 #define REG_ID 0xE3 
 #define CHIP_ID 0x9E
-
+#define REG_ENABLE 0xF8
+#define ENABLE_PON (1<<2)
+#define ENABLE_CPU_READY ( 1<<7 )
 
 static const char *TAG = "MAIN";
-
 
 esp_err_t read_reg(i2c_master_dev_handle_t dev ,uint8_t reg , uint8_t  *value ){
 
     return i2c_master_transmit_receive(dev , &reg , 1 , value ,1 ,100 ); 
+
+}
+
+esp_err_t write_reg( i2c_master_dev_handle_t dev , uint8_t reg , uint8_t value ) {
+
+    uint8_t buf[2] = {reg , value };   
+    return i2c_master_transmit(dev , buf , 2 , 100);
+
 
 }
 
@@ -93,6 +102,47 @@ if (err ==ESP_OK ){
 
 ESP_LOGI(TAG, "This is me here");
 
+err = write_reg(tmf_device , REG_ENABLE , ENABLE_PON) ;
+
+if (err == ESP_OK){
+
+    ESP_LOGI(TAG , "Writing a register is sucessful the result ");
+
+
+}
+else {
+    ESP_LOGI(TAG , "Write failed ");
+}
+
+uint8_t status = 0 ;
+
+bool ready = false ;
+
+for ( uint8_t i = 0 ; i<100 ; i++ ){
+
+    err = read_reg(tmf_device , REG_ENABLE , &status) ;
+    if (err == ESP_OK){
+    if (status & ENABLE_CPU_READY ) {
+
+    ESP_LOGI(TAG , "Device published CPU Ready");
+
+        ready = true ; 
+        break ; 
+    }   } 
+
+    else {ESP_LOGE(TAG, "Timeout Error"); 
+        break ;
+    }
+    vTaskDelay(pdMS_TO_TICKS(10));
+}
+
+if (ready == true ){
+    ESP_LOGI(TAG, "CPU is ready now ");
+}
+else{
+
+    ESP_LOGE(TAG, "Timeout Error");
+}
 
 while (1){
 
